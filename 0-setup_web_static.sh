06f9d588a92f13b2af3f9d761d1c4288da6aef80
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-07 16:08:35 +0000

    small fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,24 +15,21 @@ chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
 # Configure Nginx with a custom server block
-printf%s"server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
+printf %s "server {
+listen 80 default_server;
+listen [::]:80 default_server;
+add_header X-Served-By $HOSTNAME;
+root   /var/www/html;
+index  index.html index.htm;
+location /hbnb_static {
+alias /data/web_static/current;
+index index.html index.htm;}
+location /redirect_me {
+return 301 http://cuberule.com/;}
+error_page 404 /404.html;
+location /404 {
+root /var/www/html;
+internal;}
 }" > /etc/nginx/sites-available/default
 # Restart Nginx to apply the configuration changes
 service nginx restart
