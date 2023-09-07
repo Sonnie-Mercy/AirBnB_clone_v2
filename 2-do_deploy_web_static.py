8b0042d56dc6242bfce8e5e1937ea2c4f23912f2
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-07 17:04:40 +0000

    fixing pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,53 +6,66 @@ using the function do_deploy:
 
 import os
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
-
+from fabric.api import env, local, put, run, cd
 
 env.hosts = ['100.25.138.158', '18.214.87.67']
-
-
-def do_deploy(archive_path):
-    """Distributes archives to a server
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    dtime = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        dtime.year,
-        dtime.month,
-        dtime.day,
-        dtime.hour,
-        dtime.minute,
-        dtime.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        return None
 
 
 def do_deploy(archive_path):
     """Deploys the static files to the host servers."""
     if not os.path.exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
+        file_name = os.path.basename(archive_path)
+        folder_name = file_name.replace(".tgz", "")
+        folder_path = "/data/web_static/releases/{}/".format(folder_name)
+        # Upload the archive to the /tmp/ directory on the web server
         put(archive_path, "/tmp/{}".format(file_name))
-        run("sudo mkdir -p {}".format(folder_path))
-        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("sudo rm -rf /tmp/{}".format(file_name))
-        run("sudo mv {}web_static/* {}".format(folder_path, folder_path))
-        run("sudo rm -rf {}web_static".format(folder_path))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(folder_path))
+        # Create the release directory and unpack the archive
+        with cd("/data/web_static/releases/"):
+            run("sudo mkdir -p {}".format(folder_name))
+            run("sudo tar -xzf /tmp/{} -C {}".format(file_name, folder_name))
+
+        # Remove the temporary archive
+        run("sudo rm /tmp/{}".format(file_name))
+
+        # Move the contents to the appropriate location
+        with cd(folder_path):
+            run("sudo mv web_static/* .")
+            run("sudo rm -rf web_static")
+
+        # Update the symbolic link
+        with cd("/data/web_static/"):
+            run("sudo rm -rf current")
+            run("sudo ln -s {} current".format(folder_path))
+
         print('New version deployed!')
-        success = True
-    except Exception:
+        return True
+
+    except Exception as e:
         return False
+
+
+# Ensure that the 'versions' directory exists locally
+if not os.path.exists("versions"):
+    os.mkdir("versions")
+
+# Create a new archive and deploy it
+dtime = datetime.now()
+output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+    dtime.year,
+    dtime.month,
+    dtime.day,
+    dtime.hour,
+    dtime.minute,
+    dtime.second
+)
+local("tar -cvzf {} web_static".format(output))
+deploy_result = do_deploy(output)
+
+# Check if the deployment was successful
+if deploy_result:
+    print("Deployment successful!")
+else:
+    print("Deployment failed.")
