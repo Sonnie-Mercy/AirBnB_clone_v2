e0da7eaf28e1147702a9488484d8f53e252b45ca
sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-24 09:44:53 -0700

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that starts a web flask application to displat html data from the db"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def cities_by_states():
+    states = storage.all('State')
+    return render_template('8-cities_by_states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
